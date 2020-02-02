7d69da04712850c51d43dfabbecae04abd606a41
sanzhiev-arrival <54704867+sanzhiev-arrival@users.noreply.github.com>
2020-02-02 16:00:29 +0300

    Create R-1.1.py

---
+++ py_algorithms/chapter_1/R-1.1.py
@@ -0,0 +1,8 @@
+""" 
+
+R-1.1: Write a short Python function, is_multiple(n, m),
+that takes two intereg values and returns True if n is a 
+multiple of m, that is , n = mi for some integer i, and False otherwise.
+
+"""
+
